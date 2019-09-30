908e5695a4b8c7be823c73661b81d617c238dfd6
maddiestrange <maddie.strange@gmail.com>
2019-09-29 20:18:29 -0500

    initial setup

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
-class App extends Component {
+
+import { connect } from 'react-redux';
+import {} from '../actions'
+import SmurfCards from './SmurfCards';
+import SmurfForm from './SmurfForm';
+
+function App() {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+       <SmurfForm />
+       <SmurfCards />
       </div>
     );
   }
